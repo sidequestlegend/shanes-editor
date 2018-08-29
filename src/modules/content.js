b1571b627d88f5ea2b6bda907a73114fecc0bc26
Shane Harris <shane@xactaccounts.co.uk>
2018-08-29 11:47:31 +0100

    replaced absolute links with relative ones.

--- src/modules/content.js
+++ src/modules/content.js
@@ -56,7 +56,7 @@ export class Content{
         // Get a container view and load into the content container optionally preventing reload of the
         // content -  incase there is more to be added and we want to manually reload content later.
         await this.loadTemplates(templates)
-            .then(()=>fetch('/html/views/'+name+'.html'))
+            .then(()=>fetch('html/views/'+name+'.html'))
             .then(page=>page.text())
             .then(body=>this.container.setContent(body,noAutoReload));
     }
@@ -67,7 +67,7 @@ export class Content{
                 .map(name=>{
                     let promise = Promise.resolve();
                     if(!this.templateCache[name]){
-                        promise = fetch('/html/templates/'+name+'.html')
+                        promise = fetch('html/templates/'+name+'.html')
                             .then(page=>{
                                 this.templateCache[name] = page.text();
                             });
